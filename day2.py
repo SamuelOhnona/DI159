3454243a9eb2d88e2fbacf32cfdc28ac6af275e1
SamuelOhnona <Ohnonas@gmail.com>
2025-03-10 17:10:59 +0200

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,31 @@
+#Mandatory Exercises bellow
+
+#1
+print("Hello world\n" * 4, end="")
+
+#2
+print((99**3)*8)
+
+#3
+print(5 < 3) #False 
+print(3 == 3) #True
+print(3 == "3") #False
+#print("3" > 3) #Error
+print("Hello" == "hello")#False
+
+#4
+computer_brand = "Apple"
+print(f"I have an {computer_brand} computer")
+
+#5
+name = "Samuel"
+age = 25
+shoe_size = 44
+info = f"My name is {name}, I am {age} and my shoe size is {shoe_size}"
+print(info)
+
+#6
+a = 15
+b = 8
+if a > b:
+    print("Hello World")
